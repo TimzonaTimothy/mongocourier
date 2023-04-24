aa7cc24582901b4846d35048cb00fa3418b6f4eb
TimzonaTimothy <timzonatimothy@gmail.com>
2023-04-24 13:41:12 +0100

    main

---
+++ README.md
@@ -0,0 +1,11 @@
+# Courier-management-system
+Description:
+
+This is a simple courier management system which can be used by an organization to track packages recieved.
+
+
+# Technology used:
+
+backend: python3, django.
+
+frontend: bootstrap.
---
+++ couriermanage/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import *
+
+
+class CourierAdmin(admin.ModelAdmin):
+	list_display = ('tracking_id','sender_name', 'service_type','content',)
+	list_display_links = ('tracking_id','sender_name', 'service_type','content',)
+	# change_list_template = 'admin/change_list.html'
+	actions = None
+	readonly_fields = ('sent',)
+	list_per_page = 25
+
+class TransactionAdmin(admin.ModelAdmin):
+	list_display = ( 'parcel', 'details', 'activity','date')
+	list_per_page = 20
+	
+
+admin.site.site_header = 'EXPRESS-MONGO-ADMIN'
+admin.site.site_title = 'EXPRESS-MONGO-ADMINISTRATION'
+admin.site.register(Courier, CourierAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Contact)
+admin.site.register(Quote)
+
